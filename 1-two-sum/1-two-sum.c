f92edd77a7483b3cff2f5c034dc67ea88fdff664
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2022-03-23 23:14:57 +0300

    Time: 251 ms (11.60%), Space: 6.2 MB (99.52%) - LeetHub

--- 1-two-sum/1-two-sum.c
+++ 1-two-sum/1-two-sum.c
@@ -7,16 +7,15 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize){
     
     *returnSize = 2;
     
-    int *arr = (int*) malloc(2*sizeof(int));
+    int *arr = (int*)malloc(2 * sizeof(int));
     
-    for (int i = 0; i < numsSize-1 ; i++){
-        for (int j = i+1; j < numsSize; j++){
-            if (nums[i] + nums[j] == target){
-                arr[0] = i;
-                arr[1] = j;
-                break;
-            }
-        }
-    }
-    return arr;
+   for (int i = 0; i < numsSize ; i++){
+       for (int j = i+1 ; j < numsSize ; j++){
+           if (nums[i] + nums [j] == target){
+               arr[0] = i;
+               arr[1] = j;
+           }
+       }
+   } 
+    return arr;    
 }
