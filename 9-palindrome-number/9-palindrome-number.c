e206caffde73b27bec42d032652bd88671734883
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2022-03-26 13:24:24 +0300

    Time: 4 ms (97.47%), Space: 6.1 MB (12.37%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.c
+++ 9-palindrome-number/9-palindrome-number.c
@@ -1,13 +1,13 @@
 
 bool isPalindrome(int x){
 
-if ( x < 0 || ( x % 10 == 0 && x != 0)){
-    return false;
-}
-int revertedNumber = 0;
-while ( x > revertedNumber){
-    revertedNumber = revertedNumber *10 + x % 10;
-    x /= 10;
-}
-        return x == revertedNumber || x == revertedNumber/10;
+    if (x<0 || (x % 10 == 0) && x!=0){
+        return false;
+    }
+    int revertedNumber = 0;
+    while(x>revertedNumber){
+        revertedNumber = revertedNumber*10 + x % 10;
+        x = x/10;
+    }
+    return ((revertedNumber == x ) || (revertedNumber / 10 == x));
 }
