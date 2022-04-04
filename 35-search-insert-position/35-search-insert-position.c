1fc409fc9c5a4f71c2f603fbfab3efef424fa872
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2022-04-04 21:11:35 +0300

    Time: 7 ms (39.03%), Space: 6 MB (79.11%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.c
+++ 35-search-insert-position/35-search-insert-position.c
@@ -5,9 +5,7 @@ int searchInsert(int* nums, int numsSize, int target){
     // nums is array
     // numsSize is length
     // target is wanted
-    
     int i;
-
     for (i = 0; i < numsSize ; i++){
         
         if(nums[i] >= target){   
