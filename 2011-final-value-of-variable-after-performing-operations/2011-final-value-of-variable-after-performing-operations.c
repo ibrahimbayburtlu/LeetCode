8e2a725798076e0146d09302923f35dc73aae658
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2022-04-07 19:36:45 +0300

    Time: 19 ms (6.41%), Space: 6.8 MB (30.13%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.c
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.c
@@ -5,17 +5,17 @@ int finalValueAfterOperations(char ** operations, int operationsSize){
     // operations => + or -
     // operationsSize => length array
     
-          int x  = 0;
-          
-          for ( int i = 0 ; i < operationsSize ; i++)
-          {
-              if(*(*(operations+i)+1) == '+'){
-                x++;
-              }
-              
-              else if (*(*(operations+i)+1) != '+'){
-                  x--;
-              }
-          }
-          return x;
+    
+    int X = 0;
+    
+    for (int i = 0; i < operationsSize; i++){
+        
+        if(operations[i][1] == '+'){
+            X++;
+        }
+        else{
+            X--;
+        }
+    }
+    return X;
 }
