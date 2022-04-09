18403dcc5e3ef223a7c82af8e8b98d4d1f8ad90c
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2022-04-09 22:33:25 +0300

    Time: 19 ms (14.76%), Space: 6.5 MB (35.61%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.c
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.c
@@ -12,13 +12,13 @@ int mostWordsFound(char ** sentences, int sentencesSize){
     for (int i = 0; i < sentencesSize; i++){    // in i element we are wandering
         for (int j = 0; j < strlen(sentences[i]); j++){   
             if(sentences[i][j] == ' '){ // sentences[i][j] == *(*(sentences+i)+j) these is same 
-                count++;
+                count++;    // we are counter in space number 
             }
         }
         if (count > max){
             max = count;
         }
-        count = 0;
+        count = 0; // if we are count equat is zero we assume increase space for example, previous counter is 5 and we count new sentence it is 5+... 
     }
-    return max+1;
+    return max+1;   // for  we have 5 space okey we have 6 word because there is a space between both words.
 }
