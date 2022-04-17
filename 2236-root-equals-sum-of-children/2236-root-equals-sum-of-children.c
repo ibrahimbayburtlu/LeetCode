f17547fa68ad5c26690a648f07b3f68c79ffc95e
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2022-04-17 14:56:14 +0300

    Time: 4 ms (21.43%), Space: 6 MB (18.57%) - LeetHub

--- 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.c
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.c
@@ -8,10 +8,5 @@
  */
 bool checkTree(struct TreeNode* root){
 
-    if (root->val == (root->left->val) + (root->right->val)){
-        return true;
-    }
-    else{
-        return false;
-    }
+    return root->val == ((root->right->val) + (root->left->val));
 }
