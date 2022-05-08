d6f3247d1c807cde93434876770ed5860ae746f3
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2022-05-08 22:48:33 +0300

    Time: 150 ms (58.27%), Space: 20.7 MB (27.56%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.c
+++ 1720-decode-xored-array/1720-decode-xored-array.c
@@ -6,12 +6,12 @@
 int* decode(int* encoded, int encodedSize, int first, int* returnSize){
     
     
+    *returnSize = encodedSize + 1;
+    
     int *arr = malloc((encodedSize+1)*sizeof(int));
     
     arr[0] = first;
-    
-    *returnSize = encodedSize + 1;
-    
+        
     for(int i = 1; i <= encodedSize; i++){
         arr[i] = arr[i-1]^encoded[i-1];
     }
