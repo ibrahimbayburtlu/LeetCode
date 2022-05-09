03dc2f9747799141d2977b92853f9df0203b33ff
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2022-05-09 19:20:20 +0300

    Time: 0 ms (100.00%), Space: 5.7 MB (19.15%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.c
@@ -0,0 +1,24 @@
+
+
+char * interpret(char * command){
+    
+char *result = (char*)malloc(sizeof(char)*100);int index=0;int i=0;
+while(command[i]){
+    
+    if(command[i]=='G'){ 
+            result[index++]='G';
+            i++;
+        }
+    else if(command[i]=='(' && command[i+1]==')'){
+        result[index++]='o';
+        i=i+2;
+    }
+    else {
+       result[index++]='a';
+       result[index++]='l';
+       i=i+4;
+   }
+}
+result[index]='\0';
+return result;
+}
