ccf23c4045102d070929455f01106580f08a8c3d
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2022-05-13 18:37:58 +0300

    Time: 0 ms (100.00%), Space: 5.7 MB (22.36%) - LeetHub

---
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.c
@@ -0,0 +1,12 @@
+
+
+int xorOperation(int n, int start){
+
+    
+    int result = start;
+    
+    for(int i = 1; i < n; i++){
+        result = result ^ (start + (2*i));
+    }
+    return result;
+}
