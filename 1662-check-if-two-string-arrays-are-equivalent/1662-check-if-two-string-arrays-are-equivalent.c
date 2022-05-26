442e846f4fa2214d3c4f1e5a8cd74a6950f6e006
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2022-05-26 19:10:46 +0300

    Time: 3 ms (69.35%), Space: 6 MB (66.13%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.c
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.c
@@ -2,29 +2,31 @@
 
 bool arrayStringsAreEqual(char ** word1, int word1Size, char ** word2, int word2Size){
     
-char ans[1000];
+char arr[1000];
 int word1_len=0;
 for(int i=0;i<word1Size;i++)
 {
-    int len=strlen(word1[i]);
-    for(int j=0;j<len;j++)
+    int length=strlen(word1[i]);
+    for(int j=0;j<length;j++)
     {
-        ans[word1_len++]=word1[i][j];       
+        arr[word1_len++]=word1[i][j];       
     }   
 }
 int word2_len=0;
 
 for(int i=0;i<word2Size;i++)
 {
-    int len=strlen(word2[i]);
-    for(int j=0;j<len;j++)
+    int length=strlen(word2[i]);
+    for(int j=0;j<length;j++)
     {
-        if(ans[word2_len++]!=word2[i][j])
+        if(arr[word2_len++]!=word2[i][j])
         {
             return false;
         }          
     }   
 }
+    // word1_len = word1.length();
+    // word2_len = word2.length();
 
 if(word2_len!= word1_len)
 {
