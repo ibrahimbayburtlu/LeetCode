128c6ac6a410b23cf60e8304f746da88d85afc3e
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2022-05-29 16:15:52 +0300

    Time: 119 ms (26.86%), Space: 46.3 MB (22.86%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.js
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -2,6 +2,4 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var getConcatenation = function(nums) {
-    return[...nums,...nums];
-}
+var getConcatenation = (nums) => [...nums, ...nums]; 
