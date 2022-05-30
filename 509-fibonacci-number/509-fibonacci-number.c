cf173b1010b6521d30fcc876c95909859b137593
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2022-05-31 01:06:23 +0300

    Time: 17 ms (21.76%), Space: 5.4 MB (69.16%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.c
+++ 509-fibonacci-number/509-fibonacci-number.c
@@ -2,20 +2,7 @@
 
 int fib(int n){
 
-    int x1;
-    int x2;
-    int nextTerm;
-    if(n == 0)
-        return 0;
-    else if( n == 1)
-        return 1;
-    x1 = 0;
-    x2 = 1;
-    nextTerm = x1 + x2;
-    for(int i = 3; i <=n ; i++){
-        x1 = x2;
-        x2 = nextTerm;
-        nextTerm = x2 + x1 ;
-    }
-    return nextTerm;
+    if(n < 2)
+        return n;
+    return fib(n-1) + fib(n-2);
 }
