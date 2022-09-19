5d8c2c32d6babd4b9c007d56663a99db866733dc
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2022-09-19 11:14:35 +0300

    Time: 10 ms (8.20%), Space: 44.5 MB (57.10%) - LeetHub

---
+++ 883-projection-area-of-3d-shapes/883-projection-area-of-3d-shapes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int projectionArea(int[][] grid) {
+        int res = 0;
+        int n = grid.length;
+        for (int i = 0; i < n;i++){
+            int x = 0;
+            int y = 0;
+            for (int j = 0; j < n;++j){
+                x = Math.max(x, grid[i][j]);
+                y = Math.max(y, grid[j][i]);
+                if (grid[i][j] > 0) ++res;
+            }
+            res += x + y;
+        }
+        return res;  
+    }
+}
