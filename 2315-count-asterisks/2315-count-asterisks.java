7f02718c3fa6adb575975447e93c1442425c6f10
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2022-09-24 17:02:48 +0300

    Time: 12 ms (6.44%), Space: 42.6 MB (10.24%) - LeetHub

---
+++ 2315-count-asterisks/2315-count-asterisks.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countAsterisks(String s) {
+        int count = 0;
+        int temp = 0;
+        for (int i = 0; i < s.length();i++){
+            if (s.charAt(i) == '|'){
+                count+=1;
+            }
+            if (count % 2 == 0){
+                if (s.charAt(i) == '*'){
+                    temp += 1;
+                }
+            }
+        }
+        return temp; 
+    }
+}
