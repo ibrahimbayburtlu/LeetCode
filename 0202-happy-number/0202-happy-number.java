e15cfd402f58a511869b8a737a716257429751d1
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2023-01-21 11:10:43 +0300

    Time: 1 ms (90.71%), Space: 39.7 MB (63.35%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+    public static boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+}
