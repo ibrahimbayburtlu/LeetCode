5622baee74787dafe8c799f20716cb0e29466bdd
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2023-03-01 22:06:47 +0300

    Time: 2 ms (99.26%), Space: 41.8 MB (93.33%) - LeetHub

--- 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.java
+++ 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.java
@@ -1,12 +1,11 @@
 class Solution {
-    public double trimMean(int[] arr) {
-        double n =( 0.05 * arr.length );
+    public double trimMean(int[] arr) {        
+        double n = (0.05 * arr.length);
         Arrays.sort(arr);
-        double sum = 0;
-        for(int i = (int)n; i<arr.length-n ; i++)
-            sum = sum + arr[i];
-        double len  = arr.length - 2*n;
-        sum  = sum / len;
-        return sum;
+        double sum = 0.0;
+        for (int i = (int) n; i < arr.length - n; i++){
+            sum += arr[i];
+        }
+        return sum / (arr.length - 2*n);
     }
 }
