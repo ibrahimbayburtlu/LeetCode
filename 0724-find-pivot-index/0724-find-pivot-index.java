d2b510a5054e0d8fccdd6820dcd650a179533bba
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2023-03-09 17:54:37 +0300

    Time: 1 ms (100.00%), Space: 43.4 MB (32.06%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int leftSum = 0;
+        int rightSum = 0;
+        for(int i = 0; i < nums.length;i++){
+            rightSum += nums[i];
+        }
+        for(int i = 0; i < nums.length;i++){
+            rightSum -= nums[i];
+            if(rightSum == leftSum){
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+}
