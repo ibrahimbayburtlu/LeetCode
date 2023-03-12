475ffbb9fd44190e0373fcca6ce2d0489777024a
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2023-03-12 22:17:34 +0300

    Time: 21 ms (5.46%), Space: 43.4 MB (5.53%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -3,24 +3,24 @@ class Solution {
         String a = "";
         String b = "";
         int count = 0;
-        for (int i = 0; i < s.length();i++){
-            if (i < s.length() / 2){
-                a += s.charAt(i);
+        String lowerString = s.toLowerCase();
+        for (int i = 0; i < lowerString.length();i++){
+            if (i < lowerString.length() / 2){
+                a += lowerString.charAt(i);
             }else {
-                b += s.charAt(i);
+                b += lowerString.charAt(i);
             }
         }
-        
+
+
         for (int i = 0; i < a.length();i++){
-        if (a.charAt(i) == 'a' || a.charAt(i) == 'e' || a.charAt(i) == 'i' || a.charAt(i) == 'o'
-            || a.charAt(i) == 'u' || a.charAt(i) == 'A' || a.charAt(i) == 'E' || a.charAt(i) == 'I' || a.charAt(i) == 'O' || a.charAt(i) == 'U'){
+            if (a.charAt(i) == 'a' || a.charAt(i) == 'e' || a.charAt(i) == 'i' || a.charAt(i) == 'o' || a.charAt(i) == 'u'){
                 count ++;
             }
-            if (b.charAt(i) == 'a' || b.charAt(i) == 'e' || b.charAt(i) == 'i' || b.charAt(i) == 'o'
-            || b.charAt(i) == 'u' || b.charAt(i) == 'A' || b.charAt(i) == 'E' || b.charAt(i) == 'I' || b.charAt(i) == 'O' || b.charAt(i) == 'U') {
+            if (b.charAt(i) == 'a' || b.charAt(i) == 'e' || b.charAt(i) == 'i' || b.charAt(i) == 'o' || b.charAt(i) == 'u' ) {
                 count --;
             }
         }
-        return count == 0 ;    
+        return count == 0 ;  
     }
 }
