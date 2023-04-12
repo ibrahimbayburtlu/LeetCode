40bd31a824d8c5ddf21927b44e7ff3f3b7efd167
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2023-04-12 10:27:03 +0300

    Time: 2 ms (95.81%), Space: 43.2 MB (11.92%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+    Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        // Create an array list...
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        // Use two pointers i and j for the two arrays and initialize both with zero.
+        int i = 0, j = 0;
+        while(i < nums1.length && j < nums2.length){
+            // If nums1[i] is less than nums2[j]...
+            // Leave the smaller element and go to next(greater) element in nums1...
+            if(nums1[i] < nums2[j]) {
+                i++;
+            }
+            // If nums1[i] is greater than nums2[j]...
+            // Go to next(greater) element in nums2 array...
+            else if(nums1[i] > nums2[j]){
+                j++;
+            }
+            // If both the elements intersected...
+            // Add this element to arr & increment both i and j.
+            else{
+                arr.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        // Create a output list to store the output...
+        int[] output = new int[arr.size()];
+        int k = 0;
+        while(k < arr.size()){
+            output[k] = arr.get(k);
+            k++;
+        }
+        return output;    
+    }
+}
