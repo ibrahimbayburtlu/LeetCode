8735af1d458a52c8d213e5e413ef0f4cf6a2506b
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2023-06-03 15:57:11 +0300

    Time: 157 ms (21.26%), Space: 42 MB (86.75%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let arr = []
+    for (let i = 0; i < nums.length; i++){
+        for (let j = i+1; j < nums.length; j++){
+            if (nums[i] + nums[j] == target){
+                arr.push(i)
+                arr.push(j)
+            }
+        }   
+    }
+    return arr
+};
