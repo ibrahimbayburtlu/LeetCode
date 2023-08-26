dcf627879aa44f21fe889ed433e18896552b2bc0
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2023-08-26 20:42:39 +0300

    Time: 1 ms (38.77%), Space: 41.9 MB (6.50%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,21 +1,12 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         
-        // i = 3,4,5
-        for(int i = 0; i < n; i++){
+        // i = 0,1,2
+        for(int i = 0; i < n;i++){
             nums1[m + i] = nums2[i];
         }
+        // [1,2,3,2,5,6] non-decreasing
         Arrays.sort(nums1);
+        // [1,2,2,3,5,6] => nums1
     }
-    /*
-    class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        for i in range(n):
-            nums1[m+i]  = nums2[i]
-        nums1.sort()
-        return nums1
-    */
 }
