6c66b28117a87860c7032d035748a934f73646a2
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2023-09-10 23:07:17 +0300

    Time: 2 ms (80.63%), Space: 44.7 MB (22.74%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,11 +1,12 @@
 class Solution {
     public boolean canJump(int[] nums) {
         
-        int reachable = 0;
-       for(int i = 0; i < nums.length; i ++) {
-           if(i > reachable) return false;
-           reachable = Math.max(reachable, i + nums[i]);
-       } 
-       return true;   
+     int boundary = 0;
+     for(int i =0;i<=boundary;i++){
+         boundary = Math.max(boundary,i+nums[i]);
+         if(boundary >=nums.length-1)
+         return true;
+     } 
+     return false; 
     }
 }
