4101db6906b7719e3980fc2a3c3cd4e8a7d91ca3
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2024-02-19 15:44:01 +0300

    Time: 126 ms (5.74%), Space: 45.5 MB (51.81%) - LeetHub

---
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        int n = grid.length * grid[0].length;
+        Set<Integer> hashset = new HashSet<>();
+        int duplicateNumber = 0;
+
+        for (int[] ints : grid) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (!hashset.contains(ints[j])) {
+                    hashset.add(ints[j]);
+                } else {
+                    duplicateNumber = ints[j];
+                }
+                System.out.println(ints[j]);
+            }
+        }
+        
+        int missingNumber = 0;
+        for (int i = 0; i < n;i++){
+            if (!hashset.contains(i + 1)){
+                missingNumber = i + 1;
+            }
+        }
+        int[] arr = new int[2];
+        arr[0] = duplicateNumber;
+        arr[1] = missingNumber;
+        return arr;
+    }
+}
