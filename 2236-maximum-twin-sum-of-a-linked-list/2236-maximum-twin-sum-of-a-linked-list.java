46128ebb237bfcb896df42a10810d63b86ad84bc
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2025-10-11 19:55:56 +0300

    Time: 12 ms (13.81%), Space: 65.8 MB (20.58%) - LeetHub

--- 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.java
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.java
@@ -1,31 +1,23 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public int pairSum(ListNode head) {
+        List<Integer> nodeValue = new ArrayList<>();
         ListNode temp = head;
-        List<Integer> nodeValue = new ArrayList<Integer>();
-        int maxTwinValue = 0;
 
-        while(temp != null){
+        while (temp != null) {
             nodeValue.add(temp.val);
             temp = temp.next;
         }
+
         int start = 0;
         int last = nodeValue.size() - 1;
-        while(start < last){
-            maxTwinValue = Math.max(nodeValue.get(start) + nodeValue.get(last),maxTwinValue);
+        int maxTwinValue = 0;
+
+        while (start < last) {
+            int twinSum = nodeValue.get(start) + nodeValue.get(last);
+            maxTwinValue = Math.max(maxTwinValue, twinSum);
             start++;
             last--;
         }
-        return Math.max(maxTwinValue,nodeValue.get(nodeValue.size() / 2 - 1));
-
+        return maxTwinValue;
     }
 }
